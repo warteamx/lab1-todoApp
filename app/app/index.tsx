2bad30235ac014bbc5fb983942118ee53ceff23c
WarTeamX <martin@warteamx.com>
2025-06-06 12:27:16 +0200

    🐛 Fix express and the app

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,6 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000")
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +23,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {data ? (
+        <Text>{JSON.stringify(data, null, 2)}</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
 }
--- express/src/server.ts
+++ express/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import { createServer } from 'http';
-import app from './app';
+import { createServer } from 'http'
+import app from './app'
+
 
 const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🤪 Server is running wild on http://localhost:${PORT}`);
 });
