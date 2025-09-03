380b7cc00276edf98970c926cb926ccfd27e1d39
WarTeamX <martin@warteamx.com>
2025-09-03 11:57:52 +0200

    🔥 remove old header

--- client/components/ui/index.ts
+++ client/components/ui/index.ts
@@ -5,5 +5,4 @@ export * from './Button/Button';
 export * from './Input/Input';
 export * from './Card/Card';
 export * from './Layout/Layout';
-export * from './Header/Header';
 export * from './Navigation';
